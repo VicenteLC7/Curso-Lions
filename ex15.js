7b716ec746a0e96c7669e1ce8d59e8503be5d318
VicenteLC7 <vicente.legat@gmail.com>
2026-03-26 15:49:14 -0300

    ex15 da lista de vetores

---
+++ ex15.js
@@ -0,0 +1,14 @@
+let notas = [1, 5, 8, 9, 18, 20, 21, 24, 25, 30];
+let creccente = true;
+for (let i = 0; i < notas.length; i++) {
+    if (a[i] > a [i+1]){
+        crecente = false;
+    }
+
+}
+
+if (crecente){
+    console.log("crecente")
+} else {
+    console.log ("nao crecente")
+}
