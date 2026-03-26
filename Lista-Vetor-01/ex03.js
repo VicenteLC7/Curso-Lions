0e36a61be476ddd8cdba67c8a690aef33179914b
VicenteLC7 <vicente.legat@gmail.com>
2026-03-26 14:19:29 -0300

    inverção do exercicio 2 e 3 da lista vetor

--- Lista-Vetor-01/ex03.js
+++ Lista-Vetor-01/ex03.js
@@ -1,6 +1,7 @@
 let notas = [1, 5, 8, 9, 8];
 let soma = 0;
 for (let i = 0; i < notas.length; i++) {
-    console.log("Indice: "+ i +" Valor: "+ notas[i]);
+    soma = soma +notas[i];
     
 }
+console.log(soma);
